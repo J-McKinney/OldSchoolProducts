957241efdef04395b8e65198558367d13afec39b
Jesse <jessemckinney513@gmail.com>
2022-01-29 17:46:43 -0500

    added and connected the db, got user routes, controllers, models for auth yay

--- models/username.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const UsernameSchema = new Schema({
-  username: String,
-});
-const Username = mongoose.model("username", UsernameSchema);
-
-module.exports = Username;
--- server.js
+++ server.js
@@ -1,52 +1,63 @@
 const express = require("express");
 const app = express();
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const AuthController = require("./controllers/auth");
+const RegisterController = require("./controllers/register");
+
+const PORT = process.env.PORT || 3001;
 
 mongoose
-  .connect(
-    `mongodb://127.0.0.1:27017/OldSchoolProducts`,
-    // "mongodb+srv://jessemckinney513:Icnrmmp1@cluster0.h9eeq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(`mongodb://127.0.0.1:27017/OSPDB`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB has been connected"))
   .catch((err) => console.log(err));
 
-const UsernameModel = require("./models/username");
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully");
+});
+connection.on("error", (err) => {
+  console.log("Mongoose default connection error: " + err);
+});
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post("/api/post", async function (req, res) {
-  const { text } = req.body;
+// app.use(express.static(path.join(__dirname, "client/build")));
 
-  const newUsername = new UsernameModel({
-    username: text.toString(),
-  });
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
-  await newUsername.save();
+// app.listen(process.env.PORT || 5000);
+// module.exports = app;
+
+app.use("/api/auth", AuthController);
+app.use("/api/register", RegisterController);
+
+app.use(express.static(__dirname + "/client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
+app.listen(PORT, function () {
+  console.log(`App is running on http://localhost:${PORT}`);
 });
-
-app.listen(process.env.PORT || 5000);
-module.exports = app;
