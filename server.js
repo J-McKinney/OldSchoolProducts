b25130b297820e8e1a2f0f52996e6dce1bcc009b
Jesse <jessemckinney513@gmail.com>
2022-02-02 00:00:36 -0500

    yay

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(__dirname + "/client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
 const server = app.listen(PORT, () =>
   console.log(`Sever running on port ${PORT}`)
 );
